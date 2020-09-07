bb45f23ec9544989022941d45a23190fe295e9bc
Maksim Kovtun <brain1999@mail.ru>
2020-09-08 00:45:55 +0300

    Initial commit

---
+++ VKUser.py
@@ -0,0 +1,122 @@
+import requests
+from datetime import datetime
+
+
+class VKUser():
+    TOKEN = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
+    VKAPI_URL = "https://api.vk.com/method/"
+    PICTURE_SIZES = "wzyxrqpoms"
+
+    '''
+    s — пропорциональная копия изображения с максимальной стороной 75px;
+    m — пропорциональная копия изображения с максимальной стороной 130px;
+    x — пропорциональная копия изображения с максимальной стороной 604px;
+    o — если соотношение "ширина/высота" исходного изображения меньше или равно 3:2, то пропорциональная копия с максимальной стороной 130px. Если соотношение "ширина/высота" больше 3:2, то копия обрезанного слева изображения с максимальной стороной 130px и соотношением сторон 3:2.
+    p — если соотношение "ширина/высота" исходного изображения меньше или равно 3:2, то пропорциональная копия с максимальной стороной 200px. Если соотношение "ширина/высота" больше 3:2, то копия обрезанного слева и справа изображения с максимальной стороной 200px и соотношением сторон 3:2.
+    q — если соотношение "ширина/высота" исходного изображения меньше или равно 3:2, то пропорциональная копия с максимальной стороной 320px. Если соотношение "ширина/высота" больше 3:2, то копия обрезанного слева и справа изображения с максимальной стороной 320px и соотношением сторон 3:2.
+    r — если соотношение "ширина/высота" исходного изображения меньше или равно 3:2, то пропорциональная копия с максимальной стороной 510px. Если соотношение "ширина/высота" больше 3:2, то копия обрезанного слева и справа изображения с максимальной стороной 510px и соотношением сторон 3:2
+    y — пропорциональная копия изображения с максимальной стороной 807px;
+    z — пропорциональная копия изображения с максимальным размером 1080x1024;
+    w — пропорциональная копия изображения с максимальным размером 2560x2048px.
+    '''
+
+    def __init__(self, user_id):
+        '''Second method during Class creation'''
+        self.user_id = user_id
+
+    def __str__(self):
+        '''string representation of the Class - link to the homepage of user'''
+        return f"https://vk.com/id{str(self.user_id)}"
+
+    def print_user_id(self, user_id=None):
+        '''print user id with user = user_id (or current user if user_id is None)'''
+        print(f"User id is {self.user_id if user_id is None else user_id}")
+
+    def get_user_info(self, user_id=None):
+        '''get user description with with user = user_id (or current user if user_id is None)'''
+        parameters = {"access_token": self.TOKEN,
+                      "user_ids": self.user_id if user_id is None else user_id,
+                      "v": "5.122"}
+        response = requests.get(url=self.VKAPI_URL +
+                                "users.get", params=parameters)
+        return response.json()
+
+    def print_user_info(self, user_id=None):
+        '''print user information with user = user_id (or current user if user_id is None)
+        <user_id>: <first_name> <last_name>'''
+        user_info = self.get_user_info(
+            self.user_id if user_id is None else user_id)
+        print(
+            f"{user_info['response'][0]['id']}: {user_info['response'][0]['first_name']} {user_info['response'][0]['last_name']}")
+
+    def get_friends(self, user_id=None):
+        '''return set with friends id of  user = user_id (or current user if user_id is None)'''
+        parameters = {"access_token": self.TOKEN,
+                      "user_id": self.user_id if user_id is None else user_id,
+                      "v": "5.122"}
+        response = requests.get(url=self.VKAPI_URL +
+                                "friends.get", params=parameters)
+        return set(response.json()["response"]["items"])
+
+    def __and__(self, other_VK_user):
+        '''overriding the "&" method. Method returns a list of class' objects referring to mutual friends current user and other_VK_user'''
+        common_friends = list()
+        friends = self.get_friends() & other_VK_user.get_friends()
+
+        for friend in friends:
+            common_friends.append(VKUser(friend))
+        return common_friends
+
+    def _get_biggest_photo(self, photos_description):
+        ''' return url the biggest photo from photos_description collection'''
+
+        for size in self.PICTURE_SIZES[0:]:
+            for photo_description in photos_description:
+                if photo_description["type"] == size:
+                    # ------ EXIT!!! ------
+                    return {"size": size,
+                            "url": photo_description["url"],
+                            "file_type": photo_description["url"][photo_description["url"].rfind(".")+1:]
+                            }
+
+        return dict()  # ------ EXIT!!! ------
+
+    def get_last_n_biggest_photos(self, user_id=None, album_id="profile", num_photos=0):
+        """ Get last count=num_photos from album_id of user_id"""
+        print(f"Вконтакте: запрошены последние {num_photos} фотографий")
+        parameters = {"access_token": self.TOKEN,
+                      "user_id": self.user_id if user_id is None else user_id,
+                      "v": "5.122",
+                      "album_id": album_id,
+                      "extended": 1,
+                      "photo_sizes": 1,
+                      "rev": 1}
+        if num_photos != 0:
+            parameters["count"] = num_photos
+        response = requests.get(url=self.VKAPI_URL +
+                                "photos.get", params=parameters)
+        if response.json()["response"]["count"] == 0:
+            return list()
+        items = response.json()["response"]["items"]
+        photos = list()
+
+        for item in items:
+            photo = dict()
+            photo["likes"] = item["likes"]["count"]
+            photo["date_vk"] = item["date"]
+            photo["date_hum"] = datetime.fromtimestamp(
+                item["date"]).strftime('%Y%m%d')
+            photo["datetime_hum"] = datetime.fromtimestamp(
+                item["date"]).strftime('%Y%m%d%H%M%S')
+            photo.update(self._get_biggest_photo(item["sizes"]))
+            photo["file_name"] = str(photo["likes"]) + \
+                "." + photo["file_type"]  # likes.ext
+            photos.append(photo)
+
+        return photos
+
+
+if __name__ == '__main__':
+    kmu = VKUser(35163310)
+    photos = kmu.get_last_n_biggest_photos(num_photos=5)
+    print(photos)
---
+++ main.py
@@ -0,0 +1,65 @@
+import YaUploader
+import VKUser
+import json
+from datetime import datetime  # , timedelta
+# import time
+
+# YATOKEN = "there is must be a token for Yandex Disk"
+
+
+def prepare_list_files(photos):
+    photos_count = len(photos)
+    for photo_num in range(photos_count-1):
+        photo = photos[photo_num]
+        # print(photo)
+        # counter = 0
+        for photo_num_same_like in range(photo_num+1, photos_count - 1):
+            if photo["likes"] == photos[photo_num_same_like]["likes"]:
+                # print(photos[photo_num_same_like])
+                # counter += 1
+                photos[photo_num_same_like]["file_name"] = str(
+                    photos[photo_num_same_like]["likes"]) + f"_{photos[photo_num_same_like]['datetime_hum']}." + photos[photo_num_same_like]["file_type"]
+                photo["file_name"] = str(
+                    photo["likes"]) + f"_{photo['datetime_hum']}." + photos[photo_num_same_like]["file_type"]
+                # print("-----V")
+                # print(photos[photo_num_same_like])
+    return photos
+
+
+def get_photos_from_vk_user(vk_user, num_last_photos=5):
+    photos = vk_user.get_last_n_biggest_photos(num_photos=num_last_photos)
+    photos = prepare_list_files(photos)
+    return photos
+
+
+def save_photos_information(file_name, files_information):
+    with open(file_name, "w") as write_file:
+        json.dump(files_information, write_file, indent=4)
+    print(f"main: информация о файлах сохранена в {file_name}")
+
+
+def save_photos_to_yadisk(ya_disk, folder_name, photos):
+    ya_disk.create_folder("/" + folder_name)
+    files_information = list()
+    for photo in photos:
+        ya_disk.upload_url_file(
+            photo["url"], f"/{folder_name}/{photo['file_name']}")
+        files_information.append(
+            {"file_name": photo['file_name'], "size": photo['size']})
+    save_photos_information(folder_name + "_data.json", files_information)
+
+
+def main():
+    user_id = int(input("Введите id пользователя Вконтакте: "))
+    yandex_token = input("Введите токен Яндекса: ")
+
+    vk_user = VKUser.VKUser(user_id)
+    photos = get_photos_from_vk_user(vk_user, 5)
+
+    ya_disk = YaUploader.YaUploader(yandex_token)
+    new_folder = (
+        f"img_from_vk_id{user_id}_dt{datetime.now().strftime('%Y%m%d%H%M%S')}")
+    save_photos_to_yadisk(ya_disk, new_folder, photos)
+
+
+main()
